1ae5747567b3623d47ef62be673ec45254da2413
NikhilxKumarr <nk047647@gmail.com>
2025-10-11 01:01:00 +0530

    UI chaned for client view on tab

--- client/src/component/client.js
+++ client/src/component/client.js
@@ -1,17 +1,43 @@
 import React from "react";
 import Avatar from "react-avatar";
 
-function Client({username}) {
+function Client({ username }) {
   return (
-    <div className="d-flex align-items-center mb-3 ">
-      <Avatar
+    <div
+      className="d-flex align-items-center mb-3 px-2 py-1"
+      style={{
+        backgroundColor: "rgba(255, 255, 255, 0.05)",
+        borderRadius: "10px",
+        transition: "all 0.2s",
+        cursor: "pointer",
+      }}
+    >
+      {/* <Avatar
         name={username.toString()}
-        round="14px"
-        size={50}
-        className="mr-3"
+        round={true}
+        size="50"
+        className="me-3"
+      /> */}
+
+      <img
+        src="/images/avtar2.png"
+        style={{  width: "50px",
+          height: "50px",
+          borderRadius: "50%",
+          marginRight: "12px",
+          objectFit: "cover",
+          border: "2px solid #413d3dff", }}
       />
-      {/* <img src="/images/avtar.jpg" style={{ maxWidth: "50px" }}/> */}
-      <span className="mx-2 text-light">{username.toString()}</span>
+      <span
+        style={{
+          color: "#fff",
+          fontWeight: "600",
+          fontSize: "1rem",
+          textShadow: "0 1px 2px rgba(0,0,0,0.3)",
+        }}
+      >
+        {username.toString()}
+      </span>
     </div>
   );
 }
